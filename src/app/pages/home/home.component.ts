46bab3d0caaff491b15f979c97168e20df298132
Jordy Caro <jhordan_caro@hotmail.com>
2023-09-01 20:05:14 -0500

    creacion de servicio

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core'
-import { Title, Meta } from '@angular/platform-browser'
-import { ServicesService } from 'src/app/services/services.service'
-
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
+import { ServicesService } from 'src/app/services/services.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css'],
 })
-export class Home {
-  constructor(private title: Title, private meta: Meta,) {
-    this.title.setTitle('FIFA')
+export class Home implements OnInit {
+  equipos: any;
+  constructor(
+    private title: Title,
+    private meta: Meta,
+    // private servicesService: ServicesService
+  ) {
+    this.title.setTitle('FIFA');
     this.meta.addTags([
       {
         property: 'og:title',
         content: 'FIFA',
       },
-    ])
+    ]);
   }
+
+  ngOnInit() {
+    // this.getEquipos();
+  }
+
+  
+  // getEquipos() {
+  //   this.servicesService.getEquipos().subscribe(
+  //     (data) => {
+  //       console.log(data);
+  //       this.equipos = data.banners.reverse();
+  //     },
+  //     (error) => {
+  //     }
+  //   );
+  // }
+
 }
